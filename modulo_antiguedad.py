9d1803bbd58904c5bf3e48fd0aaeb245c6da37f7
Francisco Blazquez Weber <156860951+fblazquezweber@users.noreply.github.com>
2024-10-21 20:05:55 -0300

    Delete modulo_antiguedad.py

--- modulo_antiguedad.py
+++
@@ -1,9 +0,0 @@
-from datetime import datetime
-
-def calcular_antiguedad(fecha_ingreso):
-    fecha_actual = datetime.now()
-    fecha_ingreso = datetime.strptime(fecha_ingreso, "%d-%m-%Y")
-    antiguedad = fecha_actual.year - fecha_ingreso.year
-    if fecha_actual.month < fecha_ingreso.month or (fecha_actual.month == fecha_ingreso.month and fecha_actual.day < fecha_ingreso.day):
-        antiguedad -= 1
-    return antiguedad
