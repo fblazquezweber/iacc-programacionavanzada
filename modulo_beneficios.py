273237baedc98beed46c5057b2efead80480d7aa
Francisco Blazquez Weber <156860951+fblazquezweber@users.noreply.github.com>
2024-10-21 20:06:14 -0300

    Delete modulo_beneficios.py

--- modulo_beneficios.py
+++
@@ -1,7 +0,0 @@
-def asignar_beneficios(antiguedad):
-    if antiguedad >= 5:
-        return "Bono anual y 5 días adicionales de vacaciones"
-    elif antiguedad >= 3:
-        return "Bono anual"
-    else:
-        return "Sin beneficios adicionales"
